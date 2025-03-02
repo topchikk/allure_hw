31d9f8342eef5c01014c9b8718f06785b2cbfd97
vbiryukov <vladbiryukov0123@gmail.com>
2025-03-02 15:28:50 +0500

    базовый тест с рабочим отчетом

---
+++ tests/test_steps.py
@@ -0,0 +1,19 @@
+import allure
+from selene.support import by
+from selene.support.conditions import be
+from selene.support.shared import browser
+from selene.support.shared.jquery_style import s
+
+
+def test_github():
+    browser.open('https://github.com')
+
+    s('.header-search-button').click()
+    s('#query-builder-test').send_keys('eroshenkoam/allure-example')
+    s('#query-builder-test').submit()
+
+    s(by.link_text('eroshenkoam/allure-example')).click()
+
+    s('#issues-tab').click()
+
+    s(by.text('Тестируем тест')).should(be.visible)
